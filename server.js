b1f03def15a39c2a169bc720a33cef0df442d27f
mangala-karunarathne <mangalaicc@gmail.com>
2023-12-15 06:37:50 +0530

    CHG | Initial Commit with Project Setup

---
+++ server.js
@@ -0,0 +1,34 @@
+const dotenv = require("dotenv").config();
+const express = require("express");
+const { Logger } = require("mongodb");
+const connectDB = require("./config/connectDB")
+
+const app = express();
+const cors = require("cors");
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+app.use(
+  cors({
+    origin: [
+      "http://localhost:3000",
+      "https://mern-book-app-2ql0..onrender.com",
+    ],
+  })
+);
+
+const PORT = process.env.PORT || 5000;
+
+const startServer = async (arg) => {
+  try {
+    await connectDB();
+    app.listen(PORT, () => {
+      console.log(`Server is ruuning on port ${PORT}`);
+    });
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+module.exports = startServer();
