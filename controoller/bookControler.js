60b8cad602afbf93df8b7acf1bfb33642aa09c18
mangala-karunarathne <mangalaicc@gmail.com>
2024-01-17 21:34:46 +0530

    CHG | Update a book Data

--- controoller/bookControler.js
+++ controoller/bookControler.js
@@ -28,6 +28,14 @@ const getABook = async (req, res) => {
   }
 };
 
+const updateABook = async(req, res) => {
+  try {
+    const {id} = req.params
+  } catch (error) {
+    res.status(500).json({msg: error.message})
+  }
+}
+
 module.exports = {
   createBook,
   getAllBooks,
--- routes/bookRoute.js
+++ routes/bookRoute.js
@@ -6,5 +6,6 @@ const router = express.Router();
 router.post("/book", createBook); // Create book route
 router.get("/book", getAllBooks); // gett all books route
 router.get("/book/:id", getABook); // get a single book
+router.put("/book/:id", updateABook); // edit a single book
 
 module.exports = router;
